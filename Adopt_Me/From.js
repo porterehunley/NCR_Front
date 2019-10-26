e1a6c30f5898be071cc13d7c2ed7c384323eb91c
porterehunley <porterhunley@gatech.edu>
2019-10-26 14:12:54 -0400

    final sign in form

--- Adopt_Me/From.js
+++ Adopt_Me/From.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 
 import t from 'tcomb-form-native';
+
+<script src="http://localhost:8097"></script>
 const Form = t.form.Form;
 
 const User = t.struct({
+	name: t.String,
   email: t.String,
   username: t.String,
   password: t.String,
@@ -13,17 +16,35 @@ const User = t.struct({
 const style = StyleSheet.create({
 	container: {
     justifyContent: 'center',
-    marginTop: 50,
+    marginTop: 30,
     padding: 20,
+    width: 300,
     backgroundColor: '#ffffff',
   },
+  form_style: {
+  	width: 70,
+  }
+
 });
+
+handleSubmit = () => {
+	const value = this._form.getValue();
+  console.log('value: ', value);
+}
 
 class SignUpForm extends Component{
 	render (){
 		return(
 			<View style={style.container}>
-				<Form type={User} />
+				<Form
+					style={style.form_style} 
+					type={User} 
+					ref={c => this._form = c}
+				/>
+				<Button
+          title="Sign Up!"
+          onPress={this.handleSubmit}
+        />
 			</View>
 		);
 	}
