e0b818b444034d4ceff2e457d3288af839a42d81
porterehunley <porterhunley@gatech.edu>
2019-10-26 12:14:39 -0400

    import form

---
+++ Adopt_Me/From.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+import t from 'tcomb-form-native';
+const Form = t.form.Form;
+
+const User = t.struct({
+  email: t.String,
+  username: t.String,
+  password: t.String,
+});
+
+const style = StyleSheet.create({
+	container: {
+    justifyContent: 'center',
+    marginTop: 50,
+    padding: 20,
+    backgroundColor: '#ffffff',
+  },
+});
+
+class SignUpForm extends Component{
+	render (){
+		return(
+			<View style={style.container}>
+				<Text> AHHHHHHHHHHHHH </Text>
+			</View>
+		);
+	}
+}
+
+export default SignUpForm;
+
